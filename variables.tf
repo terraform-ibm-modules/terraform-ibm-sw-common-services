373ca3fcba42f928d507e4b6e9763adfb199a38d
Kyle Murley <kyle.murley@ibm.com>
2024-06-10 13:47:16 -0400

    Remove provider.tf and associated values

--- variables.tf
+++ variables.tf
@@ -35,35 +35,16 @@ variable "cert_manager_namespace" {
 }
 
 ####################
-# Cluster variables
-####################
-variable "cluster_id" {
-  type        = string
-  description = "The name or ID of the cluster to be used"
-}
-
-variable "ibmcloud_api_key" {
-  type        = string
-  description = "IBM Cloud API key"
-  sensitive   = true
-}
-
-variable "ibmcloud_region" {
-  type        = string
-  description = "IBM Cloud region where all resources will be deployed"
-}
-
-####################
 # Optional variables
 ####################
 variable "enable_ibm_cert_manager" {
   type        = bool
-  default     = false
+  default     = true
   description = "Option to deploy cert-manager when running terraform"
 }
 
 variable "enable_ibm_licensing" {
   type        = bool
-  default     = false
+  default     = true
   description = "Option to deploy licensing when running terraform"
 }
