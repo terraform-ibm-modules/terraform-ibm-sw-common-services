3e3c37145fe41b3c37673bdf9896955b98a5f043
Kyle Murley <kyle.murley@ibm.com>
2024-06-10 13:29:05 -0400

    Updated tests to use basic

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 /********************************************************************
 This file is used to implement the ROOT module.
 *********************************************************************/
+data "ibm_container_cluster_config" "cluster_config" {
+  cluster_name_id = var.cluster_id
+  admin           = true
+}
--- version.tf
+++ version.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.1.0"
   required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.64.0, < 2.0.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0, < 3.0.0"
