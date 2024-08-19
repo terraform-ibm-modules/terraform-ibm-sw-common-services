959fbf3e5020396b2a7081b567315d86ec99515a
Kyle Murley <kyle.murley@ibm.com>
2024-08-19 13:23:41 -0400

    Update common-dev-assets reference

--- main.tf
+++ main.tf
@@ -14,15 +14,6 @@ data "ibm_container_cluster" "cluster" {
   resource_group_id = var.cluster_resource_group_id
 }
 
-# Download cluster config which is required to connect to cluster
-/*
-data "ibm_container_cluster_config" "cluster_config" {
-  cluster_name_id   = var.cluster_id
-  resource_group_id = var.cluster_resource_group_id
-  config_dir        = "${path.module}/kubeconfig"
-  endpoint_type     = var.cluster_config_endpoint_type != "default" ? var.cluster_config_endpoint_type : null # null value represents default
-}*/
-
 locals {
   cluster_name = var.is_vpc_cluster ? data.ibm_container_vpc_cluster.cluster[0].resource_name : data.ibm_container_cluster.cluster[0].resource_name
 }
@@ -38,14 +29,6 @@ resource "helm_release" "ibm_cert_manager" {
     type  = "string"
     value = local.cluster_name
   }
-  /*
-  provisioner "local-exec" {
-    command     = "${path.module}/scripts/confirm-rollout-status.sh ${var.cert_manager_name} ${var.cert_manager_namespace}"
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      KUBECONFIG = data.ibm_container_cluster_config.cluster_config.config_file_path
-    }
-  }*/
 }
 
 resource "helm_release" "ibm_licensing" {
@@ -59,12 +42,4 @@ resource "helm_release" "ibm_licensing" {
     type  = "string"
     value = local.cluster_name
   }
-  /*
-  provisioner "local-exec" {
-    command     = "${path.module}/scripts/confirm-rollout-status.sh ${var.licensing_name} ${var.licensing_namespace}"
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      KUBECONFIG = data.ibm_container_cluster_config.cluster_config.config_file_path
-    }
-  }*/
 }
