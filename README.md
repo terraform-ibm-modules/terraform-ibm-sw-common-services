d16642bbc4b831deac85c47f165dd546ffa0d10d
Kyle Murley <kyle.murley@ibm.com>
2024-09-16 14:34:55 -0400

    Add cluster_ca_certificate to kubernetes providers

--- README.md
+++ README.md
@@ -79,12 +79,14 @@ provider "helm" {
   kubernetes {
     host  = data.ibm_container_cluster_config.cluster_config.host
     token = data.ibm_container_cluster_config.cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
 }
 
 provider "kubernetes" {
   host  = data.ibm_container_cluster_config.cluster_config.host
   token = data.ibm_container_cluster_config.cluster_config.token
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
 # ############################################################################
--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -4,14 +4,16 @@
 
 provider "helm" {
   kubernetes {
-    host  = data.ibm_container_cluster_config.cluster_config.host
-    token = data.ibm_container_cluster_config.cluster_config.token
+    host                   = data.ibm_container_cluster_config.cluster_config.host
+    token                  = data.ibm_container_cluster_config.cluster_config.token
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
 }
 
 provider "kubernetes" {
-  host  = data.ibm_container_cluster_config.cluster_config.host
-  token = data.ibm_container_cluster_config.cluster_config.token
+  host                   = data.ibm_container_cluster_config.cluster_config.host
+  token                  = data.ibm_container_cluster_config.cluster_config.token
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
 provider "ibm" {
